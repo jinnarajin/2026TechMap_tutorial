30aaecca0f8c60dd248ce78d09ee425e42928575
jinnarajin <seoleeb26@pos.idserve.net>
2026-08-31 16:37:29 +0900

    chore: add visionOS hand tracking project scaffold

---
+++ HandTracking/handtracking/handtracking/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  handtracking
+//
+//  Created by Seojin Lee on 8/26/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
